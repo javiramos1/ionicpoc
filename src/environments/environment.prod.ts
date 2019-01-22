7db1bef71dbb66b7a938bdea9032735e4900558c
javier <javier.ramos1@gmail.com>
2019-01-22 17:12:34 +0000

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB60OGIAQs7ptc2TQmdCZkunSiNN9a0QaU",
-    authDomain: "ionicpoc-il.firebaseapp.com",
-    databaseURL: "https://ionicpoc-il.firebaseio.com",
-    projectId: "ionicpoc-il",
-    storageBucket: "ionicpoc-il.appspot.com",
-    messagingSenderId: "371791543705"
+
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB60OGIAQs7ptc2TQmdCZkunSiNN9a0QaU",
-    authDomain: "ionicpoc-il.firebaseapp.com",
-    databaseURL: "https://ionicpoc-il.firebaseio.com",
-    projectId: "ionicpoc-il",
-    storageBucket: "ionicpoc-il.appspot.com",
-    messagingSenderId: "371791543705"
+
   }
 };
 
